999045dd0892d58e30c1ce73e9c159fcd0850307
Cyanide <miqueas736d@gmail.com>
2026-04-09 20:09:57 -0300

    add: adding base files for page

---
+++ front-react/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import NavComponent from "./components/NavComponent/NavComponent"
+import FooterComponent from "./components/FooterComponent/FooterComponent"
+import MainForm from "./components/MainForm/MainForm";
+
+
+function App() {
+  return (
+    <>
+    <NavComponent/>
+    <MainForm/>
+    <FooterComponent/>
+    </>
+  )
+}
+
+
+export default App;
