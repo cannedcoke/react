7b226a2362b2de25ce6f1a95df7975e1dc1914b2
Cyanide <miqueas736d@gmail.com>
2026-04-10 21:16:38 -0300

    feat: adding conditional rendering of pages

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -1,19 +1,40 @@
 import "./App.css";
-
-import NavComponent from "./components/NavComponent/NavComponent"
-import FooterComponent from "./components/FooterComponent/FooterComponent"
+import { useState } from "react";
+import NavComponent from "./components/NavComponent/NavComponent";
+import FooterComponent from "./components/FooterComponent/FooterComponent";
 import MainForm from "./components/MainForm/MainForm";
-
+import Details from "./components/DetailsComponent/DetailsComponent";
 
 function App() {
+  const [currentView, SetCurrentView] = useState("main");
+  const [selectedId, SetSelectedId] = useState(null);
+
+  function handleSelectDetail(id) {
+    SetSelectedId(id);
+    SetCurrentView("detail");
+  }
+
+  if (currentView === "detail") {
+    return (
+      <>
+        <NavComponent />
+        <main>
+          <Details id={selectedId} onBack={() => SetCurrentView("main")} />
+        </main>
+        <FooterComponent />
+      </>
+    );
+  }
+
   return (
     <>
-    <NavComponent/>
-    <MainForm/>
-    <FooterComponent/>
+      <NavComponent />
+      <main>
+        <MainForm onSelectDetail={handleSelectDetail} />
+      </main>
+      <FooterComponent />
     </>
-  )
+  );
 }
-
 
 export default App;
